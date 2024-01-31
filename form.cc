be9fe495d6deceb020260de54ed863de43a9bede
Justis Mencarelli <jwmencarelli95@gmail.com>
2024-01-31 15:08:21 -0800

    Modified more files

--- form.cc
+++ form.cc
@@ -36,7 +36,7 @@ int main() {
     cout << "NOTE: Your partners' grade will not be impacted based on your responses." << endl << endl << endl;
 
     //ifstream students("/public/spring24_csci41.txt"); // This file is the Spring 2024 CSCI 41 class.
-    ifstream students("spring24_csci41.txt"); // This file is the Spring 2024 CSCI 41 class.
+    ifstream students("/public/spring24_csci41.txt"); // This file is the Spring 2024 CSCI 41 class.
     if (!students) throw runtime_error("Ensure the file for students is in your /public directory.\n"); // if this file does not exist, throw.
 
     string env = getenv("HOME");
@@ -59,7 +59,7 @@ int main() {
         }
     }
     unordered_map<int, Group> groupDB;
-    ifstream groups("testGroups.txt");
+    ifstream groups("/public/milestoneOne.txt");
     if (!groups) throw runtime_error("Invalid file for groups\n");
     int groupNumber = 0;
     string temp;
